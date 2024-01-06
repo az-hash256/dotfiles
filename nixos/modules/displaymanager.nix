83468b63a1a9f03b9ea8b97be8077b637195a7d1
az-hash256 <arellafi@bu.edu>
2024-01-06 11:10:45 -0500

    changed sddm theme

--- nixos/modules/displaymanager.nix
+++ nixos/modules/displaymanager.nix
@@ -1,14 +1,14 @@
 # Configures keymap in X11, enables displaymanager and XFCE.
 {
   services = {
-   xserver = {
+    xserver = {
       layout = "us";
       xkbVariant = "";
       enable = true;
       displayManager = {
-      sddm.enable = true;
-      sddm.theme = "/usr/share/sddm/themes/catppuccin-mocha";
+        sddm.enable = true;
+        sddm.theme = "/usr/share/sddm/themes/sddm-sugar-dark";
+      };
     };
-   };
   };
 }
